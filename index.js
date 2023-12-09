6679bc43d88918b889d2e2a9342b2a2ae3fab591
alex-njuguna <njugunakinuthia013@gmail.com>
2023-12-09 23:47:12 +0300

    move dodger to the right

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const dodger = document.getElementById('dodger')
 
 dodger.style.backgroundColor = '#ff69b4'
 
+
 function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace('px', '')
     const left = parseInt(leftNumbers, 10)
@@ -11,11 +12,20 @@ function moveDodgerLeft() {
     }
 }
 
+function moveDodgerRight(){
+    const rightNumbers = dodger.style.left.replace('px', '')
+    const right = parseInt(rightNumbers, 10)
 
+    if (right > 0 || right === 0){
+        dodger.style.left = `${right + 1}px`
+    }
+}
 
 document.addEventListener('keydown', function(event){
     if (event.key === 'ArrowLeft'){
         moveDodgerLeft()
+    } else if (event.key === 'ArrowRight'){
+        moveDodgerRight()
     }
 })
 
